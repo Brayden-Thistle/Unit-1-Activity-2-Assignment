614ead05459dbbcbde91e701d50cfe75ecd82fd3
default <bthistle@huronperthcatholic.ca>
2025-10-06 15:42:31 +0000

    added a program that converts temperature

---
+++ Program1/main.go
@@ -0,0 +1,27 @@
+
+/*
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-10-06
+* @fileoverview this program converts temperature
+*/
+
+
+package main
+
+
+import "fmt"
+
+
+func main () {
+ //question
+fmt.Println("What is 34 degrees Celsius in fahrenheit?")
+
+//calculation
+fmt.Println("34 degrees Celsius is equal to " + fmt.Sprint(9 / 5 * 34 + 32) + "°F")
+
+
+fmt.Println("\nDone")
+
+
+}
---
+++ Program1/main.ts
@@ -0,0 +1,13 @@
+/**
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-10-06
+* @fileoverfiew this program converts temperature
+*/
+
+
+console.log("What is 34 degrees Celsius in fahrenheit?");
+console.log("34 degrees celsius is equal to " + (9 / 5 * 34 + 32) + "°F");
+
+
+console.log("\nDone");
