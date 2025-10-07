a7997286a2659e785ee8b44cb90a61079a7ed6c2
default <bthistle@huronperthcatholic.ca>
2025-10-07 13:41:22 +0000

    added comments

--- Program1/main.ts
+++ Program1/main.ts
@@ -5,9 +5,9 @@
 * @fileoverfiew this program converts temperature
 */
 
-
+//this is the question
 console.log("What is 34 degrees Celsius in fahrenheit?");
+// this is the answer to the equation and the answer to the question.
 console.log("34 degrees celsius is equal to " + (9 / 5 * 34 + 32) + "°F");
-
 
 console.log("\nDone");
