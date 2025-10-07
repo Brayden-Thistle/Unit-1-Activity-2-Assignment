f2d6f29df06d04989f05fab495024b6454f5f3aa
default <bthistle@huronperthcatholic.ca>
2025-10-07 13:57:00 +0000

    added a program that outputs the area of a circle

---
+++ Program2/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview this program computes and outputs the area of a circle
+ */
+
+//This is the initial question
+console.log("What is the area of a circle with a radius of 14cm?");
+
+//this is the equation and the answer
+console.log("The area of a circle with a radius of 14cm is " + (3.14 * 14 * 14) + "cm²");
+
+console.log("\nDone");
---
+++ Program2/main2.go
@@ -0,0 +1,20 @@
+/*
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview this program computes and outputs the area of a circle
+ */
+
+package main
+
+import "fmt"
+
+func main () {
+// this is the initial question to the equation
+fmt.Println("What is the area of a circle with the radius of 14cm?")
+
+//this is the answer to the question
+fmt.Println("The area of a cirlce with the radius of 14cm is " + fmt.Sprint(3.14 * 14 * 14) + "cm²")
+
+fmt.Println("\nDone")
+}
