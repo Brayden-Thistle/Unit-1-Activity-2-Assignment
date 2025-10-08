24860414c1ccfc28aa44eb9501bbbd2eb9106cee
default <bthistle@huronperthcatholic.ca>
2025-10-08 02:04:47 +0000

    added a program that calculates the gross pay for an employee named Fred

---
+++ Program4/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview this program calculates the gross pay for an employee
+ */
+
+//this is my equation
+console.log("How much is Fred getting paid to work 40 hours this week for $8.20 per hour?");
+
+//this is my solution
+console.log("Fred is getting paid $" + (40 * 8.2) + " for this week.");
+
+console.log("\nDone");
---
+++ Program4/main4.go
@@ -0,0 +1,21 @@
+/*
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview this program calculates the gross pay for an employee
+ */
+
+package main
+
+import "fmt"
+
+func main () {
+	//this is my equation
+	fmt.Println("How much is Fred getting paid for working 40 hours this week at a wage of $8.20 per hour?")
+
+	//this is my solution
+	fmt.Println("Fred is getting paid $" + fmt.Sprint(40 * 8.2) + " for working 40 hours this week with his wage of $8.20 an hour.")
+
+	fmt.Println("\nDone")
+
+}
